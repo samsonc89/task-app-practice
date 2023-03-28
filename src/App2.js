30b0bd8762c53dbdc205a80d84f270af60beead1
Samson Cheung <samsonc89@yahoo.com>
2023-03-28 13:19:00 -0700

    Add function to submit tasks

--- src/App2.js
+++ src/App2.js
@@ -4,19 +4,40 @@ import MyComponent from "./components/Overview2";
 class App extends Component {
   constructor() {
     super();
+
+    this.state = { task: "", tasks: [] };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ task: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: "",
+    });
+    console.log(this.state.tasks);
   }
 
   render() {
     return (
       <div>
-        <label for="task">
-          Task
-          <input type="text" name="task" />
-        </label>
-        <button type="submit" onSubmit="">
-          World
-        </button>
-        <MyComponent text="Hello" title="WOrk" />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Task
+            <input
+              type="text"
+              value={this.state.task}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">World</button>
+        </form>
+        <MyComponent text={this.state.tasks} title="WOrk" />
       </div>
     );
   }
--- src/components/Overview2.js
+++ src/components/Overview2.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class MyComponent extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
   }
   render() {
     return (
